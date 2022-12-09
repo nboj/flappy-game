df745a50b0bcf46e9cec882db8e968762ae77996
Christian <christian.auman@outlook.com>
2022-12-09 16:45:17 -0500

    update
    Added Character creator

--- MenuScene-classes/CharacterCreator.js
+++ MenuScene-classes/CharacterCreator.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+import hexToHsl from 'hex-to-hsl'
 
 class CharacterCreator {
 	constructor(scene) {
@@ -10,6 +11,11 @@ class CharacterCreator {
 		this.startingBirdColor = startingBirdColor
 		this.currentBirdColor = startingBirdColor
 		this.colorPicker = null
+		this.birdRenderer = this.createBird()
+	}
+	
+	createBird() {
+	
 	}
 	
 	preload() {
@@ -37,66 +43,19 @@ class CharacterCreator {
 				size: 16,
 			},
 			valuechangeCallback: (newValue, oldValue, colorPicker) => {
-				const color = colorPicker.childrenMap.hPalette.childrenMap.paletteCanvas.colorObject
-				const HSL = this.RGBToHSL(color.r, color.g, color.b)
-				console.log(HSL)
-				this.pipelineInstance.setHueRotate(43/355)
-				this.pipelineInstance.setSatAdjust(100)
-				this.pipelineInstance.setLumAdjust(0.6)
-				// const HSL = this.hexToHSL(-value)
-				// pipelineInstance.setHueRotate(HSL.h)
-				// pipelineInstance.setSatAdjust(HSL.s)
-				// pipelineInstance.setLumAdjust(HSL.l)
-				// "rgba(255,0,14,1)"
+				const color = colorPicker.childrenMap.svPalette.childrenMap.paletteCanvas.colorObject
+				let s = color.s
+				let h = 1-color.h+0.52
+				let v = color.v
+				this.pipelineInstance.setHueRotate(h)
+				this.pipelineInstance.setSatAdjust(s * 1.5)
+				this.pipelineInstance.setLumAdjust(v *.55)
 			}
 		})
 			.layout()
 	}
 	
-	RGBToHSL(r,g,b) {
-		// Make r, g, and b fractions of 1
-		r /= 255;
-		g /= 255;
-		b /= 255;
-		
-		// Find greatest and smallest channel values
-		let cmin = Math.min(r,g,b),
-			cmax = Math.max(r,g,b),
-			delta = cmax - cmin,
-			h = 0,
-			s = 0,
-			l = 0;
-		// Calculate hue
-		// No difference
-		if (delta == 0)
-			h = 0;
-		// Red is max
-		else if (cmax == r)
-			h = ((g - b) / delta) % 6;
-		// Green is max
-		else if (cmax == g)
-			h = (b - r) / delta + 2;
-		// Blue is max
-		else
-			h = (r - g) / delta + 4;
-		
-		h = Math.round(h * 60);
-		
-		// Make negative hues positive behind 360°
-		if (h < 0)
-			h += 360;
-		// Calculate lightness
-		l = (cmax + cmin) / 2;
-		
-		// Calculate saturation
-		s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-		
-		// Multiply l and s by 100
-		s = +(s * 100).toFixed(1);
-		l = +(l * 100).toFixed(1);
-		
-		return {h:h, s:s, l:l}
-	}
+	
 }
 
 export default CharacterCreator
